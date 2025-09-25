e4e2c69cb4d74cb59a29b2d7ff3d65098f2e2303
aleksnet5 <aleksnet.pam@gmail.com>
2025-09-25 16:41:55 +0200

    test5

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';  // <-- add
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],                     // <-- add
   providers: [provideHttpClient()],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']          // <-- plural
 })
 export class AppComponent {
   apiBase = '/api';
